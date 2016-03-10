df8f3e845aa03816b5e06238b15737dc74f1ccc1
meshnesh <tonnie.nesh@gmail.com>
2016-03-10 20:27:00 +0300

    added the app intro

---
+++ app/src/main/java/com/example/entsfrank/kaa_fit/MainIntro.java
@@ -0,0 +1,72 @@
+package com.example.entsfrank.kaa_fit;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+
+/**
+ * Created by tune on 10/03/16.
+ */
+public class MainIntro  extends AppIntro {
+
+    // Please DO NOT override onCreate. Use init.
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(SampleSlide.newInstance(R.layout.first_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.second_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.third_fragment));
+
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+//        addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
+        Toast.makeText(getApplicationContext(), getString(R.string.skip), Toast.LENGTH_SHORT).show();
+        loadMainActivity();
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+        loadMainActivity();
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something when the slide changes.
+    }
+
+    @Override
+    public void onNextPressed() {
+        // Do something when users tap on Next button.
+    }
+}
